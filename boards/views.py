31626874f17736b658feddd755031f24e915dbbf
wendyorengo <wenndyorengo@gmail.com>
2020-08-24 01:14:43 +0300

    add a form rendering option

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from .models import Board
 from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 def home(request):
     boards = Board.objects.all()
@@ -15,8 +17,22 @@ def board_topics(request,pk):
 
 def new_topic(request,pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'new_topic.html', {'board':board})
-
-   
+    user = User.objects.first()  
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
     
 
